9c6e52b22bf5f5dd99863130925f03328007ce31
nikopapp <np15685@my.bristol.ac.uk>
2018-05-24 22:47:27 +0100

    Read transactions

--- src/com/nikopapp/writer/CsvWriter.java
+++ src/com/nikopapp/writer/CsvWriter.java
@@ -12,9 +12,24 @@ public class CsvWriter implements Writer {
 
     @Override
     public void writeFile(List<Transaction> transactions, String path) throws IOException {
-        List<String> lines = transactions.stream().map(x ->
-                String.format("%s|%s|%s", x.getDate(), x.getDescription(), x.getAmount())
-        ).collect(Collectors.toList());
+        List<String> lines = transactions.stream()
+                .filter(CsvWriter::filter)
+                .map(x ->
+                        String.format("%s|%s|%s", x.getDate(), x.getDescription(), x.getAmount())
+                ).collect(Collectors.toList());
         Files.write(Paths.get(path), lines);
     }
+
+    private static boolean filter(Transaction transaction) {
+        if (transaction.getDescription().toLowerCase().contains("starthclyde")) {
+            System.out.println("found Starchcly "+transaction.getDescription());
+            return false;
+        }
+        if (transaction.getDescription().toLowerCase().contains("regular sav")) {
+            System.out.println("found RegularSaver "+transaction.getDescription());
+            return false;
+        }
+        return true;
+    }
+
 }
